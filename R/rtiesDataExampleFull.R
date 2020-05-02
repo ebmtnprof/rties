fd5dd5bc9d34373b163b339d19ee950d0a6e655c
Emily Butler <eabutler@u.arizona.edu>
2020-05-02 13:33:28 -0700

    Version 5.0.0

---
+++ R/rtiesDataExampleFull.R
@@ -0,0 +1,5 @@
+
+#' Data for examples in the vignettes.
+#'
+#' A dataset containing variables for the examples in the vignettes.
+"rties_ExampleDataFull"
--- R/rtiesDataExtra.R
+++
@@ -1,5 +0,0 @@
-
-#' Data for demonstrating rties models.
-#'
-#' A dataset containing a minimal set of variables for demonstrating rties analyses.
-"rties_ExampleData_ExtraVars"
